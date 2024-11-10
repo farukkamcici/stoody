da63bf5784b674c13de26cc41a860b9b1b5ecce6
Faruk Kamçıcı <farukkamcici@gmail.com>
2024-11-10 22:12:43 +0300

    ios added

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,15 @@
 package io.ionic.starter;
 
+import com.google.firebase.FirebaseApp;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+@Override
+    public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    FirebaseApp.initializeApp(this);
+}
+}
+
+
