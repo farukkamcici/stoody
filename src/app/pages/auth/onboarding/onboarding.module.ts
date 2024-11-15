ca4733dd514f6c4d68a3c3c2e5aa2b24a6e2ad43
Faruk Kamçıcı <farukkamcici@gmail.com>
2024-11-15 08:47:20 +0300

    onboarding v1 rev

--- src/app/pages/auth/onboarding/onboarding.module.ts
+++ src/app/pages/auth/onboarding/onboarding.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
@@ -15,6 +15,8 @@ import { OnboardingPage } from './onboarding.page';
     IonicModule,
     OnboardingPageRoutingModule
   ],
-  declarations: [OnboardingPage]
+  declarations: [OnboardingPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
-export class OnboardingPageModule {}
+export class OnboardingPageModule { }
--- src/app/pages/auth/onboarding/onboarding.page.ts
+++ src/app/pages/auth/onboarding/onboarding.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,12 +7,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./onboarding.page.scss'],
 })
 export class OnboardingPage {
+  @ViewChild('swiperRef', { static: false }) swiperRef: any; // Swiper referansı
+
+  public currentSlide = 0;
+  public totalSlides = 3;
 
   constructor(
     private router: Router
   ) { }
 
+  ionViewDidEnter() {
+    setTimeout(() => {
+      const swiperEl = document.querySelector('swiper-container') as any;
+      if (swiperEl && swiperEl.swiper) {
+        swiperEl.swiper.slideTo(0);
+        swiperEl.swiper.on('slideChange', () => {
+          this.currentSlide = swiperEl.swiper.activeIndex;
+          console.log('Aktif slayt:', this.currentSlide);
+        });
+      } else {
+        console.error('Swiper yüklenmedi.');
+      }
+    }, 100);
+  }
+
   public navigateToSignIn() {
     this.router.navigate(['auth/sign-in'])
+  }
+
+  handleNextClick(swiper: any) {
+    if (this.currentSlide === this.totalSlides - 1) {
+      this.navigateToSignIn();
+    } else {
+      swiper.swiper.slideNext();
+    }
   }
 }
