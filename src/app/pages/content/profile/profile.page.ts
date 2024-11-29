6d44d5ad6ed781c14920f74bd337892c53fa18eb
Faruk Kamçıcı <farukkamcici@gmail.com>
2024-11-29 20:32:17 +0300

    proifle tab v1

--- src/app/pages/content/profile/profile.page.ts
+++ src/app/pages/content/profile/profile.page.ts
@@ -8,13 +8,31 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage {
+  public user: any = null; // Initialize user as null
 
   constructor(
     private authService: AuthService,
     private router: Router
-  ) { }
+  ) {}
+
+  ionViewDidEnter() {
+    this.getUser();
+  }
 
   public async signOut() {
-    this.authService.signOut();
+    await this.authService.signOut();
+    this.router.navigate(['/login']); // Redirect to login after sign-out
+  }
+
+  public getUser() {
+    this.authService.getCurrentUser().subscribe({
+      next: (user) => {
+        this.user = user; // Assign the user details to the `user` property
+        console.log('User:', this.user);
+      },
+      error: (err) => {
+        console.error('Error fetching user:', err);
+      },
+    });
   }
 }
